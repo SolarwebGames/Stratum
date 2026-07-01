2911fd128bb9d04ff232ecc0f9b30f4febb83b8e
Dameon Laird <26032268+DameonL@users.noreply.github.com>
2026-06-30 17:32:48 -0700

    Minor: Allow telescope use underneath skylights

---
+++ Source/JoyGivers/UseTelescope.cs
@@ -0,0 +1,30 @@
+using Verse;
+using RimWorld;
+using SolarWeb.Stratum.Stats;
+
+namespace SolarWeb.Stratum.JoyGivers;
+
+public class UseTelescope : JoyGiver_InteractBuildingInteractionCell
+{
+  protected override bool CanInteractWith(Pawn pawn, Thing t, bool inBed)
+  {
+    if (def.unroofedOnly && t.Spawned)
+    {
+      var roof = t.Map.roofGrid.RoofAt(t.Position);
+      if (roof != null && RoofStatCache.IsSkylight(roof))
+      {
+        bool originalUnroofedOnly = def.unroofedOnly;
+        def.unroofedOnly = false;
+        try
+        {
+          return base.CanInteractWith(pawn, t, inBed);
+        }
+        finally
+        {
+          def.unroofedOnly = originalUnroofedOnly;
+        }
+      }
+    }
+    return base.CanInteractWith(pawn, t, inBed);
+  }
+}
